d7160f47b03dbbfa62d5eecc61d6dc6cef2710b2
LobaDK <lobadk@lobadk.com>
2022-07-08 00:03:30 +0200

    Added code to move file and wait 10 seconds between each upload

--- looper.py
+++ looper.py
@@ -1,8 +1,12 @@
 import glob
 from pathlib import Path
-import os   
+import os
+import time
 path = 'D:\\OBS\\GTA5\\edited\*.mp4'
 files = glob.glob(path)
 files.sort(key=os.path.getmtime)
 for file in files:
     os.system(f'python VideoUploader.py --file="{file}" --title="{Path(file).stem}" --description="" --category="20" --privacyStatus="private"')
+    os.rename(file,os.path.join('D:\\OBS\\GTA5\\tempupload',os.path.basename(file)))
+    print('Waiting 10 seconds...')
+    time.sleep(10)
